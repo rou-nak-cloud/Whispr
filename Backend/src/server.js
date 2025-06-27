140f78d9ae3bdbb6f8c09968881e45f0e7c27f16
rou-nak-cloud <github.rounak.19@gmail.com>
2025-06-27 14:51:56 +0530

    production V9

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -23,7 +23,7 @@ const __dirname = path.resolve();
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-    app.use("*", (req,res) => {
+    app.get("/*", (req,res) => {
         res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
         // res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
     })
