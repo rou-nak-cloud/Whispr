5fee58686b675195f85f3ba2780ae5de58270db6
rou-nak-cloud <github.rounak.19@gmail.com>
2025-06-27 12:50:20 +0530

    production V5

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -21,7 +21,8 @@ dotenv.config({ path: './.env' })
 
 const __dirname = path.resolve();
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    // app.use(express.static(path.join(__dirname, "../frontend/dist")));
+      res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 
     app.use("*", (req,res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
