20c4ebd45d11ef559fe716e206649a4301fa9ac3
rou-nak-cloud <github.rounak.19@gmail.com>
2025-06-28 14:00:22 +0530

    serverRectify

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,66 +1,7 @@
-// import dotenv from 'dotenv'
-
-// import express from 'express';
-// import path from 'path'
-// import { fileURLToPath } from "url";
-
-// import cors from 'cors';
-// import cookieParser from 'cookie-parser';
-// import { app } from './lib/socket.js';
-
-// //  Apply all middleware
-// app.use(cors({
-//   origin: 'http://localhost:5173',
-//   credentials: true,
-// }));
-// app.use(express.json({ limit: '10mb' }));
-// app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-// app.use(cookieParser());
-
-// dotenv.config({ path: './.env' })
-// // dotenv.config();
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// if(process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-//     app.get("*", (req,res) => {
-//          console.log("Wildcard route hit");
-//         res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-//         // res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-//     })
-// }
-
-// // Import and register routes directly
-// import authRoutes from './routes/auth.route.js';
-// import messageRoutes from './routes/message.route.js';
-
-// app.use('/api/auth', authRoutes);
-// app.use('/api/messages', messageRoutes);
-
-
-// import { server } from './lib/socket.js'; // from socket io is now "main"
-// import connectDB from './lib/db.server.js'
-// // console.log("MongoDB URI:", process.env.MONGODB_URI);
-
-// connectDB()
-// .then(() => {
-//     app.on('error', (error) => {
-//         console.log('Error in DB listening connection', error)
-//         throw error
-//     })
-//     server.listen(process.env.PORT || 8000, () => {
-//         console.log(`Server is running on port ${process.env.PORT}`)
-//     })
-// }).catch((err) => {
-//     console.log('MongoDB connection failed', err)
-// });
-
 
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -72,9 +13,9 @@ import { server } from './lib/socket.js';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
-// const __dirname = path.resolve();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // Setup
 dotenv.config({ path: './.env' });
